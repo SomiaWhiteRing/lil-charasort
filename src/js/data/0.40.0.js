1483cbd1e3e36cca28174f45cec128961103b290
苍旻 <55022033+SomiaWhiteRing@users.noreply.github.com>
2024-05-23 10:57:05 +0800

    LiL Version

---
+++ src/js/data/0.40.0.js
@@ -0,0 +1,290 @@
+dataSetVersion = "2021-05-09"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Cast Type",
+    key: "cast",
+    checked: false,
+    sub: [
+      { name: "Sensei", key: "sensei" },
+      { name: "Main Cast", key: "main" },
+      { name: "Side Cast", key: "side" },
+    ]
+  },
+  {
+    name: "Remove MILF",
+    key: "milf",
+    checked: false
+  },
+  {
+    name: "Remove Loli",
+    key: "loli",
+    checked: false
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Sensei",
+    img: "Sensei.png",
+    opts: {
+      cast: ["sensei"]
+    }
+  },
+  {
+    name: "Chika Chosokabe",
+    img: "chikathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Yumi Yamaguchi",
+
+    img: "yumithumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Ayane Amamiya",
+    img: "ayanethumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Sana Sakakibara",
+    img: "sanathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Makoto Miyamura",
+    img: "makotothumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Miku Maruyama",
+    img: "mikuthumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Futaba Fukuyama",
+    img: "futabathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Rin Rokuhara",
+    img: "rinthumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Ami Arakawa",
+    img: "amithumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Maya Makinami",
+    img: "mayathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Molly MacCormack",
+    img: "mollythumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Tsuneyo Tojo",
+    img: "tsuneyothumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Uta Ushibori",
+    img: "utathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Io Ichimonji",
+    img: "iothumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Nodoka Nagasawa",
+    img: "nodokathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Otoha Okakura",
+    img: "otohathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Touka Tsukioka",
+    img: "toukathumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Yasu Yasui",
+    img: "yasuthumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Noriko Nakayama",
+    img: "norikothumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Kirin Kanda",
+    img: "kirinthumb1.png",
+    opts: {
+      cast: ["main"]
+    }
+  },
+  {
+    name: "Sara Sakakibara",
+    img: "sarathumb1.png",
+    opts: {
+      cast: ["side"],
+      milf: true
+    }
+  },
+  {
+    name: "Haruka Hamasaki",
+    img: "harukathumb1.png",
+    opts: {
+      cast: ["side"],
+      milf: true
+    }
+  },
+  {
+    name: "Kaori Kadowaki",
+    img: "kaorithumb1.png",
+    opts: {
+      cast: ["side"]
+    }
+  },
+  {
+    name: "Chinami Chosokabe",
+    img: "chinamithumb1.png",
+    opts: {
+      cast: ["side"],
+      loli: true
+    }
+  },
+  {
+    name: "Karin Kanda",
+    img: "karinthumb1.png",
+    opts: {
+      cast: ["side"]
+    }
+  },
+  {
+    name: "Maki Miyamura",
+    img: "makithumb1.png",
+    opts: {
+      cast: ["side"],
+      milf: true
+    }
+  },
+  {
+    name: "Yuki Yamaguchi",
+    img: "yukithumb1.png",
+    opts: {
+      cast: ["side"],
+      milf: true
+    }
+  },
+  {
+    name: "Niki Nakayama",
+    img: "nikithumb1.png",
+    opts: {
+      cast: ["side"]
+    }
+  },
+  {
+    name: "Wakana Watabe",
+    img: "wakanathumb1.png",
+    opts: {
+      cast: ["side"]
+    }
+  },
+  {
+    name: "Osako Osaka",
+    img: "osakothumb1.png",
+    opts: {
+      cast: ["side"]
+    }
+  },
+  {
+    name: "Tsubasa Tsukioka",
+    img: "tsubasathumb1.png",
+    opts: {
+      cast: ["side"],
+      milf: true
+    }
+  },
+  {
+    name: "Tsukasa Tsukioka",
+    img: "tsukasathumb1.png",
+    opts: {
+      cast: ["side"],
+      loli: true
+    }
+  },
+  {
+    name: "Imani Imai",
+    img: "imanithumb1.png",
+    opts: {
+      cast: ["side"]
+    }
+  },
+  {
+    name: "Rika Rokuhara",
+    img: "rikathumb1.png",
+    opts: {
+      cast: ["side"],
+      milf: true
+    }
+  },
+  {
+    name: "Nao-chan",
+    img: "naothumb1.png",
+    opts: {
+      cast: ["side"],
+      loli: true
+    }
+  }
+];
